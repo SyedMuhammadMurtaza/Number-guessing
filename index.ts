71b8148edba9880e1fa233444c212af1695dcc94
SyedMuhammadMurtaza <smmhj19@gmail.com>
2024-04-07 00:09:52 +0500

    Number guessing game first push

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        message:"Guess any number between 1 to 6",
+        type:"number",
+        name:"Number"
+    }    
+]);
+if (answer.Number === randomNumber) {
+    console.log("Correct guess");
+}
+else { 
+    console.log("Wrong guess");
+}
+
